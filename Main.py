f36909cae1addc8934a2907e17a1a2b9baa04f81
Briankr33 <Briankr28@gmail.com>
2018-03-13 11:07:22 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import re
 from openpyxl import Workbook
+import Test
+
+file_to_read = 'Regex_Test'
 
 #Open .ctx file and read contents
-readFile = open("Regex_Test", "r")
-readList = readFile.read()
-readFile.close()
+with open(file_to_read, 'r') as f:
+    readList = f.read()
 
 #Define regex patterns
 regexGroup = r"GmmiLinkContextFrameObject\s+.+\s+.+\s+.+\s+.+\s+.+\s+.+\s+.+\s+.+\s+.+\s+.+"
@@ -14,6 +16,7 @@ regexDescription = r"\"Desc2\"\s\".+\""
 
 #Declare and initialize variable for list of regex matches
 matchList = re.findall(regexGroup, readList)
+print(matchList)
 
 #Create Excel workbook and assign variable for active sheet
 wb = Workbook()
@@ -27,23 +30,28 @@ for _ in range(listLength):
     columnLetter = 'A'
     strRowNumber = str(rowNumber)
     matchItem1 = re.findall(regexBit, matchList[_])
-    if len(matchItem1) != 0:
-        valueColumnA = matchItem1[0]
+    print(matchItem1)
+    if matchItem1:
+        if len(matchItem1) > 1:
+            valueColumnA = ", ".join(matchItem1)
+        else:
+            valueColumnA = matchItem1[0]
+        print(valueColumnA)
         ws1["%s%s" % (columnLetter, strRowNumber)] = valueColumnA
 
     columnLetter = 'B'
     matchItem2 = re.findall(regexDevice, matchList[_])
-    if len(matchItem2) != 0:
-        print(matchItem2)
+    if matchItem2:
         valueColumnB = matchItem2[0]
         ws1["%s%s" % (columnLetter, strRowNumber)] = valueColumnB
 
     columnLetter = 'D'
     matchItem3 = re.findall(regexDescription, matchList[_])
-    print(len(matchItem3))
-    if len(matchItem3) != 0:
+    if matchItem3:
         valueColumnC = matchItem3[0]
         ws1["%s%s" % (columnLetter, strRowNumber)] = valueColumnC
 
 #Save excel workbook
+print("Code finished successfully.")
 wb.save('Practice2.xlsx')
+print("Workbook saved.")
